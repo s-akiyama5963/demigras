e31fc4190c4c75afa267e7581cb35ba33c72228c
Sumi.Ug1999 <develop@sumiug.com>
2024-08-19 18:39:51 +0900

    共通オプションを実装する。

--- bin/main.php
+++ bin/main.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputOption;
 
 // Setup
 require_once(realpath(__DIR__.'/../resource/constants.php'));
@@ -8,6 +9,22 @@ require_once(realpath(__DIR__.'/../vendor/autoload.php'));
 
 $app = new Application('Demigras', DEMIGRAS_VERSION);
 $app->setVersion(DEMIGRAS_VERSION);
+
+
+// 共通オプション追加
+$options = [];
+$definition = $app->getDefinition();
+
+$options[] = new InputOption(
+    '--config',
+    null,
+    InputOption::VALUE_REQUIRED,
+    '設定ファイルの場所を指定する。',
+    '.env',
+);
+
+$definition->addOptions($options);
+$app->setDefinition($definition);
 
 // コマンド追加
 $app->addCommands([
