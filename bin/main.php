8ad00f181c044ccb3f1113e2c775fcb5fae35dbc
Sumi.Ug1999 <develop@sumiug.com>
2024-08-19 19:24:52 +0900

    設定ファイルの読み込み処理を実装する。

--- bin/main.php
+++ bin/main.php
@@ -1,7 +1,11 @@
 <?php
 
+use Dotenv\Dotenv;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 // Setup
 require_once(realpath(__DIR__.'/../resource/constants.php'));
@@ -25,6 +29,36 @@ $options[] = new InputOption(
 
 $definition->addOptions($options);
 $app->setDefinition($definition);
+
+
+// 共通処理定義
+$dispatcher = new EventDispatcher();
+
+$dispatcher->addListener(ConsoleEvents::COMMAND, function(ConsoleCommandEvent $event) : void
+{
+    $input = $event->getInput();
+
+    // 環境変数設定
+    $dotenvPath = $input->getOption('config');
+    $dotenvName = null;
+
+    if($dotenvPath === null)
+    {
+        $dotenvPath = getcwd();
+    }
+    else
+    {
+        $dotenvPathPart = pathinfo($dotenvPath);
+        $dotenvPath = $dotenvPathPart['dirname'];
+        $dotenvName = $dotenvPathPart['basename'];
+    }
+
+    $dotenv = Dotenv::createImmutable($dotenvPath, $dotenvName);
+    $dotenv->load();
+});
+
+$app->setDispatcher($dispatcher);
+
 
 // コマンド追加
 $app->addCommands([
